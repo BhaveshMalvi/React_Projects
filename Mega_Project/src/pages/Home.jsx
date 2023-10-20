7888abac59f6e93252cc99b12e63e457f49ab5c2
BhaveshMalvi <bhaveshkumarmalvi87@gmail.com>
2023-10-20 20:12:53 +0530

    UI Update1.0

--- Mega_Project/src/pages/Home.jsx
+++ Mega_Project/src/pages/Home.jsx
@@ -29,9 +29,9 @@ function Home() {
         )
     }
     return (
-        <div className='w-full py-8'>
+        <div className='w-full py-8 '>
             <Container>
-                <div className='flex flex-wrap'>
+                <div className='flex flex-wrap over'>
                     {posts.map((post) => (
                         <div key={post.$id} className='p-2 w-1/3'>
                             <PostCard {...post} />
